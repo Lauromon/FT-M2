ff9eabc20b5000b94d3652714f2d3ba99932eef6
Lautaro <orbesle@gmail.com>
2022-10-03 23:32:24 -0300

    ok

--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -5,7 +5,15 @@ import './Nav.css';
 
 function Nav({onSearch}) {
   return (
-    <div>Nav</div>
+    <div>
+      <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href='https://www.soyhenry.com/'>
+        <img src={Logo} width='30px' height='30px' alt='img not found'/>
+        Henry - Weather App
+      </a>
+      <SearchBar onSearch={onSearch}/>
+    </nav>
+    </div>
   );
 };
 
--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 
 export default function SearchBar({onSearch}) {
+  const [city, setCity] = useState('')
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      onSearch("Cairns");
+      onSearch(city);
     }}>
-      <input
+      <input class="form-control me-2" onChange={(e) => {setCity(e.target.value)}}
         type="text"
         placeholder="Ciudad..."
       />
-      <input type="submit" value="Agregar" />
+      <button class="btn btn-outline-success" type="submit">Agregar</button>
     </form>
   );
 }
